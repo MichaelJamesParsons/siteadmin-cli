eae9e4bec330565beb7175e14ea2a3f94de3f56c
Michael-James Parsons <Michael-James Parsons>
2017-03-21 07:17:54 -0400

    Refactored provisioning script

--- provision.sh
+++ provision.sh
@@ -1,56 +1,138 @@
 #!/usr/bin/env bash
 
-sudo yum -y update
-sudo yum -y install wget
+#
+# Installs libraries required to provision the OS
+#
+function install_dependencies() {
+    sudo yum -y install wget
+}
 
-# Install Git
-sudo yum -y install git
+#
+# Upgrades OS and updates installed packages
+#
+function update_packages() {
+    sudo yum -y dist-upgrade
+    sudo yum -y update
+}
 
-# Install Ruby Version Manager (RVM) [https://rvm.io/rvm/install]
-curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
-sudo \curl -sSL https://get.rvm.io | bash -s stable --ruby
-sudo gem install bundler
+#
+# Installs Git globally
+#
+function install_git() {
+    # Install Git
+    sudo yum -y install git
+}
 
-# TODO - Pull siteadmin-cli from Git
-# TODO - Execute bundler in siteadmin-cli install directory
-sudo bundle install
+#
+# Installs ruby 2.4.* and bundler (package manager)
+#
+function install_ruby() {
+    # Install Ruby Version Manager (RVM) [https://rvm.io/rvm/install]
+    curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
+    \curl -sSL https://get.rvm.io | bash -s stable --ruby
+    gem install bundler
 
-# Install PHP 7.0
-sudo yum -y install epel-release
-sudo wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
-sudo wget https://centos6.iuscommunity.org/ius-release.rpm
-sudo rpm -Uvh ius-release*.rpm
+    # TODO - Pull siteadmin-cli from Git
+    # TODO - Execute bundler in siteadmin-cli install directory
+    bundle install
+}
 
-sudo yum install -y http://dl.iuscommunity.org/pub/ius/stable/CentOS/7/x86_64/ius-release-1.0-14.ius.centos7.noarch.rpm
+#
+# Installs PHP 7.0.*
+#
+function install_php() {
+    sudo yum -y install epel-release
+    wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+    sudo wget https://centos6.iuscommunity.org/ius-release.rpm
+    sudo rpm -Uvh ius-release*.rpm
 
-sudo yum -y install php70u php70u-pdo php70u-mysqlnd php70u-opcache php70u-xml php70u-mcrypt php70u-gd php70u-devel php70u-mysql php70u-intl php70u-mbstring php70u-bcmath php70u-json php70u-iconv
+    sudo yum install -y http://dl.iuscommunity.org/pub/ius/stable/CentOS/7/x86_64/ius-release-1.0-14.ius.centos7.noarch.rpm
 
-sudo yum-config-manager --enable http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+    sudo yum -y install php70u php70u-pdo php70u-mysqlnd php70u-opcache php70u-xml php70u-mcrypt
+    sudo yum -y install php70u-gd php70u-devel php70u-mysql php70u-intl php70u-mbstring php70u-bcmath
+    sudo yum -y install php70u-json php70u-iconv
+}
 
-# Install composer
-cd /tmp
-curl -sS https://getcomposer.org/installer | php
-mkdir -p /usr/local/bin
-sudo mv composer.phar /usr/local/bin/composer
+#
+# Install composer globally
+#
+function install_composer {
+    cd /tmp
+    curl -sS https://getcomposer.org/installer | php > /dev/null
+    mkdir -p /usr/local/bin
+    sudo mv composer.phar /usr/local/bin/composer
+}
 
-# Install apache server
-sudo yum install httpd -y
+#
+# Install apache2
+#
+function install_apache2() {
+    # Install apache2 server
+    sudo yum install httpd -y
 
-# Start apache server
-sudo service httpd start
+    # Start apache server
+    sudo service httpd start
 
-# Install mysql
-sudo yum -y install mariadb-server mariadb
+    # Start httpd service on boot
+    sudo systemctl enable httpd
+}
 
-# Start mysql
-sudo systemctl start mariadb
+#
+# Installs maria DB
+#
+function install_maria_db() {
+    # Install mysql
+    sudo yum -y install mariadb-server mariadb
 
-# Configure mysql
-sudo mysql -e "UPDATE mysql.user SET Password = PASSWORD('root') WHERE User = 'root'"
-sudo mysql -e "DROP USER ''@'localhost'"
-sudo mysql -e "DROP USER ''@'$(hostname)'"
-sudo mysql -e "DROP DATABASE IF EXISTS test"
-sudo mysql -e "FLUSH PRIVILEGES"
+    # Start mysql
+    sudo systemctl start mariadb
 
-# Start mysql when VM bootssudo mysql -e "FLUSH PRIVILEGES"
-sudo systemctl enable mariadb.service
+    # Start mysql when VM boots
+    sudo systemctl enable mariadb.service
+}
+
+#
+# Configures the root database user
+#
+function configure_root_db_user() {
+    # Set root password to 'root'
+    sudo mysql -e "UPDATE mysql.user SET Password = PASSWORD('root') WHERE User = 'root'"
+
+    # Drop anonymous users and hosts
+    sudo mysql -e "DROP USER ''@'localhost'"
+    sudo mysql -e "DROP USER ''@'$(hostname)'"
+
+    # Drop test database
+    sudo mysql -e "DROP DATABASE IF EXISTS test"
+
+    # Commit changes
+    sudo mysql -e "FLUSH PRIVILEGES"
+}
+
+
+install_dependencies
+
+# Install git if not found
+if ! rpm -qa | grep -qw git; then
+    install_git
+fi
+
+# Install composer if not found
+if ! whereis composer | grep -qw /usr/bin/composer; then
+    install_composer
+fi
+
+# Install composer if not found
+if ! whereis httpd | grep -qw /usr/sbin/httpd; then
+    install_apache2
+fi
+
+# Install maria DB if not found
+if ! rpm -qa | grep -qw mariadb-server; then
+    install_maria_db
+fi
+
+# Install php if not found
+if ! whereis composer | grep -qw /usr/bin/php; then
+    install_php
+fi
